6ccd2a29d4f3dc8aea0560f82b8807ab62c73c3b
Dominik Schmidtlein <dominik.schmidtlein@gmail.com>
2016-03-29 13:48:19 -0400

    fix bugs

--- CarletonExamSupport/src/carletonexamsupport/GUI.java
+++ CarletonExamSupport/src/carletonexamsupport/GUI.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package examservice;
+package carletonexamsupport;
 
 import javax.swing.JCheckBox;
 import javax.swing.JFileChooser;
